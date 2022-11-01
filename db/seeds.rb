5b30f677765360db1d139fb1aa1065f85863b6ee
Dirk Mischke <dee2577@gmail.com>
2022-11-01 20:16:59 +0100

    added user id and image_url and seed content to paintings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Painting.new(name: "Mona Lisa", description: "Painted between 1503 and 1517, Da Vinci’s alluring portrait has been dogged by two questions since the day it was made: Who’s the subject and why is she smiling?", price: 100, image_url: "/app/assets/images/Mona_Lisa.jpg")
+Painting.new(name: "Girl with a Pearl", description: "Johannes Vermeer’s 1665 study of a young woman is startlingly real and startlingly modern, almost as if it were a photograph.", price: 90, image_url: "/app/assets/images/Girl_with_a_Pearl.jpg")
+Painting.new(name: "The starry Night", description: "Vincent Van Gogh’s most popular painting, The Starry Night was created by Van Gogh at the asylum in Saint-Rémy, where he’d committed himself in 1889.", price: 95, image_url: "/app/assets/images/The_starry_Night.jpg")
+Painting.new(name: "The Kiss", description: "Opulently gilded and extravagantly patterned, The Kiss, Gustav Klimt’s fin-de-siècle portrayal of intimacy, is a mix of Symbolism and Vienna Jugendstil, the Austrian variant of Art Nouveau.", price: 100, image_url: "/app/assets/images/The_Kiss.jpg")
