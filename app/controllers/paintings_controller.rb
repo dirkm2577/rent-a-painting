5d0e307632ee814888bd7fa8f6e41411120b0497
Alexander Morris <alexmorrisjobs@gmail.com>
2022-11-01 19:07:06 +0000

    added new and create methods with minimal html on the view page

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -4,6 +4,18 @@ class PaintingsController < ApplicationController
   end
 
   def create
-    
+    @painting = Painting.new(painting_params)
+    if @painting.save
+      redirect_to paintings_path
+      # redirect_to paintings_path(@painting)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:name, :price, :description)
   end
 end
