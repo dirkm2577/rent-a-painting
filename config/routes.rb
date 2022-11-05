7863cb8616f29a1d0ca1af8e34c9c9d67cc34576
Alexander Morris <alexmorrisjobs@gmail.com>
2022-11-05 10:28:49 +0000

    created MVC for bookings - added empty user methods and many to many associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/show'
   devise_for :users
   root to: "paintings#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :paintings, only: [:index, :new, :create]
+  resources :paintings, only: [:index, :new, :create] do
+    resources :bookings
+  end
 end
