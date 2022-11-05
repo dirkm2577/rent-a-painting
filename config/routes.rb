236c76eada6b3a3ddfd8ce4ed9656d79428d142a
Dirk Mischke <dee2577@gmail.com>
2022-11-05 14:38:40 +0100

    last changes before half time demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/show'
   devise_for :users
-  root to: "paintings#index"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
