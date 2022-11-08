d50d9c4fafd4f2463e5dfc1f667758745c20f4a3
Dirk Mischke <dee2577@gmail.com>
2022-11-08 20:08:09 +0100

    small change to image tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ require "open-uri"
 # Painting.new(name: "The starry Night", description: "Vincent Van Gogh’s most popular painting, The Starry Night was created by Van Gogh at the asylum in Saint-Rémy, where he’d committed himself in 1889.", price: 95, image_url: "/app/assets/images/The_starry_Night.jpg")
 # Painting.new(name: "The Kiss", description: "Opulently gilded and extravagantly patterned, The Kiss, Gustav Klimt’s fin-de-siècle portrayal of intimacy, is a mix of Symbolism and Vienna Jugendstil, the Austrian variant of Art Nouveau.", price: 100, image_url: "/app/assets/images/The_Kiss.jpg")
 Painting.destroy_all
+User.destroy_all
 
 user1 = User.new(email: "test@test.com", password: "123456")
 
@@ -20,7 +21,7 @@ file = URI.open("https://res.cloudinary.com/dfe5bng4a/image/upload/v1667745513/M
 painting = Painting.new(
   name: "Mona Lisa",
   description: "The Mona Lisa is an oil painting by Italian artist, inventor, and writer Leonardo da Vinci. Likely completed in 1506, the piece features a portrait of a seated woman set against an imaginary landscape. In addition to being one of the most famous paintings, it is also the most valuable.",
-  price: 83400000,
+  price: 80,
   user: user1
 )
 painting.photo.attach(io: file, filename: "mona_lisa.jpg", content_type: "image/jpg")
@@ -33,7 +34,7 @@ painting = Painting.new(
   The work is oil on canvas and is 44.5 cm (17.5 in) high and 39 cm (15 in) wide. It is signed 'IVMeer' but not dated. It is estimated to have been painted around 1665.
   After the most recent restoration of the painting in 1994, the subtle colour scheme and the intimacy of the girl's gaze toward the viewer have been greatly enhanced. During the restoration, it was discovered that the dark background, today somewhat mottled, was originally a deep enamel-like green. This effect was produced by applying a thin transparent layer of paint—a glaze—over the black background seen now. However, the two organic pigments of the green glaze, indigo and weld, have faded.
   ",
-  price: 30100000,
+  price: 100,
   user: user1
 )
 painting.photo.attach(io: file, filename: "girl_with_a_pearl_earring.jpg", content_type: "image/jpg")
